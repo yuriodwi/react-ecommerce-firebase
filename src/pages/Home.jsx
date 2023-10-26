6ad92c67ec3c82e3666105b56d312ab97c43b198
Yurio Dwi LS <rea.riera@gmail.com>
2023-10-26 14:31:06 +0700

    custom slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,44 @@
+import Banner from "../components/Banner";
+
 const Home = () => {
-  return <div>Home</div>;
+  const data = [
+    "https://amazonproone.vercel.app/static/media/img2.bc1bdb910ead16c65197.jpg",
+    "https://amazonproone.vercel.app/static/media/img5.aa945e25375bfdee385f.jpg",
+    "https://amazonproone.vercel.app/static/media/img3.c80809bb40bee5c34372.jpg",
+    "https://amazonproone.vercel.app/static/media/img1.efb3d39101f7ef77d616.jpg",
+  ];
+  return (
+    <div className="w-full h-auto">
+      <div>
+        <div className="flex w-[400vw] h-full">
+          <img
+            className="w-screen h-full object-cover"
+            src={data[0]}
+            alt="ImageOne"
+            loading="priority"
+          />
+          <img
+            className="w-screen h-full object-cover"
+            src={data[1]}
+            alt="ImageTwo"
+            loading="priority"
+          />
+          <img
+            className="w-screen h-full object-cover"
+            src={data[2]}
+            alt="ImageThree"
+            loading="priority"
+          />
+          <img
+            className="w-screen h-full object-cover"
+            src={data[3]}
+            alt="ImageFour"
+            loading="priority"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
