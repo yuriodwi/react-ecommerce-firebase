ed811e7a7a7323d636ec1278a2b1148f84d7a3ce
Yurio Dwi LS <rea.riera@gmail.com>
2023-10-27 11:12:39 +0700

    fix image productCard

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
 const ProductsCard = ({ product }) => {
   return (
-    <div>
-      <div>
-        <img src={product.image} alt="productImage" />
+    <div className="group">
+      <div className="w-full h-96 cursor-pointer overflow-hidden">
+        <img
+          className="w-full h-full object-cover group-hover:scale-110 duration-300"
+          src={product.image}
+          alt="productImage"
+        />
       </div>
     </div>
   );
