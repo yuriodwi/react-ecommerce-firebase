277de52dd1b3a6c309601b6750bd3926cf06a1cc
Yurio Dwi LS <rea.riera@gmail.com>
2023-10-27 10:42:40 +0700

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="font-bodyFont">
-      <Header />
-      <Home />
-      <Footer />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { cartImg, logoDark } from "../assets";
 
 const Header = () => {
   return (
-    <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-tittleFont">
+    <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-tittleFont sticky top-0 z-50">
       <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
         <div>
           <img className="w-28" src={logoDark} alt="logoDark" />
